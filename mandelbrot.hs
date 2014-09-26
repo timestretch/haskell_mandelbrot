e9e62c130659d4bb14d903279fbca95604800df9
Erik Wrenholt <erik@timestretch.com>
2014-09-26 18:08:29 -0500

    clean up

--- mandelbrot.hs
+++ mandelbrot.hs
@@ -10,31 +10,31 @@ mandelbrot cr ci =
 		let zr2 = zr * zr in
 		let zi2 = zi * zi in
 		if zi2 + zr2 > 16.0 || i >= 1000 
-		then i
-		else iter (i+1) (zr2 - zi2 + cr_alt) (temp + temp + ci) in
+			then i
+			else iter (i+1) (zr2 - zi2 + cr_alt) (temp + temp + ci) in
 	iter 0 0.0 0.0 
 
 
 calc :: Int -> Int -> Int
 calc x y = 
-	mandelbrot ((fromIntegral x)/40.0) ((fromIntegral y)/40.0)
+	let scale n = (fromIntegral n)/40.0 in
+	mandelbrot (scale x) (scale y)
 
 
 pixel_val :: Int -> Int -> String
 pixel_val x y = 
 	if calc x y == 1000 
-	then "*" 
-	else " "
+		then "*" 
+		else " "
 
 range :: [Int]
 range = [-39..39]
 
 get_row :: Int -> String
-get_row x = 
-	concat (map (pixel_val x) range)
+get_row x = concat $ map (pixel_val x) range
 
 all_rows :: String
-all_rows = intercalate "\n" (map get_row range)
+all_rows = intercalate "\n" $ map get_row range
 
 main :: IO ()
 main = putStrLn $ all_rows
